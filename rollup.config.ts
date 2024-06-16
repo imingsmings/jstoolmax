936bd020f7f9aef8865c688176e8eb951aaba3fa
Jason Wang <imings@icloud.com>
2024-06-17 01:15:21 +0800

    fix build

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,6 +2,7 @@ import typescript from 'rollup-plugin-typescript2'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import { dts } from 'rollup-plugin-dts'
 
 const outputConfigs = [
     {
@@ -20,10 +21,22 @@ const outputConfigs = [
     }
 ]
 
-export default outputConfigs.map((config) => ({
-    input: 'src/index.ts',
-    output: {
-        ...config
-    },
-    plugins: [typescript(), resolve(), commonjs(), terser()]
-}))
+export default [
+    ...outputConfigs.map((config) => ({
+        input: 'src/index.ts',
+        output: {
+            ...config
+        },
+        plugins: [
+            typescript(),
+            resolve(),
+            commonjs(),
+            terser()
+        ]
+    })),
+    {
+        input: './src/index.ts',
+        output: [{ file: 'dist/index.d.ts', format: 'es' }],
+        plugins: [dts()]
+    }
+]
